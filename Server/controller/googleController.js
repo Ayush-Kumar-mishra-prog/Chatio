20ed3479717b40f026dbde30fc8da9a5404bee81
Ayush kumar Mishra <ayushrajmishra960@gmail.com>
2026-06-06 13:12:39 +0530

    4th commit

--- Server/controller/googleController.js
+++ Server/controller/googleController.js
@@ -1,30 +1,45 @@
-import { oauth2client } from "../configs/googleConfig.js"
-import GUser from "../models/google.model.js"
-import jwt from 'jsonwebtoken'
+import { oauth2client } from "../configs/googleConfig.js";
+import { google } from "googleapis";
+import GUser from "../models/google.model.js";
+import jwt from "jsonwebtoken";
 
-const googleLogin = async (req,res)=>{
-    try {
-        const { code } = req.body
-        const googleRes = await oauth2client.getToken(code)
-        oauth2client.setCredentials(googleRes.tokens)
-
-        const userRes = await axios.get(`https://www.googleapis.com/oauth2/v3/userinfo?alt=json&access_token=${googleRes.tokens.access_token}`, )
-        const {email,name,picture} = userRes.data
-        let user = await GUser.findOne({email});
-        if(!user){
-            user = await GUser.create({
-                name,email,image:picture
-            })
-        }
-        const {_id}= user
-        const token= jwt.sign({_id,email},process.env.JWT_SECRET,{
-            expiresIn:process.env.JWT_TIMEOUT
-        })
-        return res.status(200).json({message:"success" ,token,user})
-    } catch (error) {
-        console.error("Error fetching Google access token:", error)
-        res.status(500).json({ error: "Failed to fetch Google access token" })
+const googleLogin = async (req, res) => {
+  try {
+    const { code } = req.query;
+    if (!code) {
+      return res
+        .status(400)
+        .json({ error: "Missing Google authorization code" });
     }
-}
 
-export default googleLogin
+    const googleRes = await oauth2client.getToken(code);
+    oauth2client.setCredentials(googleRes.tokens);
+
+    const oauth2 = google.oauth2({ auth: oauth2client, version: "v2" });
+    const userRes = await oauth2.userinfo.get();
+    const { email, name, picture } = userRes.data;
+
+    let user = await GUser.findOne({ email });
+    if (!user) {
+      user = await GUser.create({
+        name,
+        email,
+        image: picture,
+      });
+    }
+
+    const { _id } = user;
+    const token = jwt.sign({ _id, email }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_TIMEOUT,
+    });
+
+    return res.status(200).json({ message: "success", token, user });
+  } catch (error) {
+    console.error("Error fetching Google access token:", error);
+    res
+      .status(500)
+      .json({ error: error.message || "Failed to fetch Google access token" });
+  }
+};
+
+export default googleLogin;
